235fd338effded42860323fb93171a66115b5a5b
TTS <52875256+technooxy@users.noreply.github.com>
2023-01-11 21:18:46 +0800

    UPDATED ENV

--- hello/gradleBuild.sh
+++ hello/gradleBuild.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # this is a comment !
 echo "starting the build for microservice"
+export GRADLE_HOME=/opt/gradle/gradle-6.9.1
+export PATH=${GRADLE_HOME}/bin:${PATH}
 gradle clean build -x test
